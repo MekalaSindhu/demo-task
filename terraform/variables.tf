b22626840525ac0a5050490dd319ce0d85a29d92
MEKALA SAI SINDHU <mekalasaisindhu@gmail.com>
2025-11-01 22:52:20 +0530

    updated variable.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,17 +1,25 @@
 variable "region" {
-  description = "AWS region"
-  type        = string
-  default     = "us-east-1"
+  description = "AWS region to deploy resources"
+  default     = "us-west-1"
+}
+
+variable "instance_type" {
+  description = "EC2 instance type"
+  default     = "t3.medium"
+}
+
+variable "key_name" {
+  description = "Existing AWS key pair name"
+  default     = "my-keypair"
 }
 
 variable "vpc_cidr" {
-  description = "CIDR block for VPC"
-  type        = string
+  description = "VPC CIDR block"
   default     = "10.0.0.0/16"
 }
 
 variable "subnet_cidr" {
-  description = "CIDR block for subnet"
-  type        = string
+  description = "Subnet CIDR block"
   default     = "10.0.1.0/24"
 }
+
